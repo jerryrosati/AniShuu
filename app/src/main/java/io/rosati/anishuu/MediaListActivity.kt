d204f68df8199ecf35f30796a2e2507711d5b07f
Jerry <jerry@rosati.io>
2020-05-03 17:01:23 -0400

    First Commit

---
+++ app/src/main/java/io/rosati/anishuu/MediaListActivity.kt
@@ -0,0 +1,12 @@
+package io.rosati.anishuu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MediaListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media_list)
+    }
+}
