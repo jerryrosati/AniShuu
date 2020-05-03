36eefad2cf2667410ae05e1abb607a072a9aae7f
Jerry <jerry@rosati.io>
2020-05-03 19:13:46 -0400

    Started adding navigation component

--- app/src/main/java/io/rosati/anishuu/MainActivity.kt
+++ app/src/main/java/io/rosati/anishuu/MainActivity.kt
@@ -1,20 +1,12 @@
 package io.rosati.anishuu
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val button = findViewById<Button>(R.id.button1)
-        button.setOnClickListener {
-            startActivity(Intent(this, MediaListActivity::class.java))
-        }
     }
 }
